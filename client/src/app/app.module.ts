23c72313d1024b4a8f702c68be6ddc02a7b2f1e6
Jacques Thurling <jthurling78@gmail.com>
2021-07-23 04:59:05 +0200

    Update: Added services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { AccountsComponent } from './dashboard/accounts/accounts.component';
+import { TransfersComponent } from './dashboard/transfers/transfers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    SideNavComponent
+    SideNavComponent,
+    AccountsComponent,
+    TransfersComponent
   ],
   imports: [
     BrowserModule,
